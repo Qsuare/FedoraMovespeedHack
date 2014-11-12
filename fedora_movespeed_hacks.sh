5d9df1c8c565d6ca3055aefcaf2adc68f91d575d
shpitzick <maorkdosh@gmail.com>
2014-11-12 21:52:30 +0200

    added hacks

---
+++ fedora_movespeed_hacks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+echo -e -n "\e[31m"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+
+echo -e -n "\e[0m"
+echo -e "xXx WELCOME TO THE FEDORA MOVESPEED HACK 100% SAFE AND RELIABLE xXx"
+
+echo -e -n "\e[31m"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+
+echo -e -n "\e[0m \n"
+echo -n "Enter your birth date: "
+read birthdate
+firefox http://i.imgur.com/1LCMBfH.jpg
